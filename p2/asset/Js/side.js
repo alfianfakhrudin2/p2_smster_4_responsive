2c12fcde5570c036bd61f83f5581daac39f8f1ab
Alfian fakhrudin <alfian4551@gmail.com>
2023-04-27 02:30:34 +0700

    update halaman dashboard serta sidebar

---
+++ p2/asset/Js/side.js
@@ -0,0 +1,26 @@
+
+var myIndex = 0;
+slidebar();
+
+function slidebar(){
+    var i;
+    var x = document.getElementsByClassName("card-image");
+    for(i = 0; i < x.length; i++){
+        x[i].style.display = "none";
+    }
+
+    myIndex++;
+    if(myIndex > x.length){
+        myIndex = 1;
+    }
+    x[myIndex-1].style.display = "block";
+    setTimeout(slidebar, 2500);
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
