e6286c7634e916e46286a3e34625795ef6742cbb
AlfianJe <zenvrog@gmail.com>
2023-04-29 10:36:56 +0700

    update, darkmode udah aktif

--- p2/asset/Js/login.js
+++ p2/asset/Js/login.js
@@ -1,51 +1,39 @@
 $(document).ready(function()
 {
-$("#login").click(function()
-{
-
-	var uid = $("#uid").val();
-	var password = $("#password").val();
-	
-	//checking for blank fields
-	if( uid =='' || password =='')
-		{
-		  $('input[type="text"],input[type="password"]').css("border","2px solid red");
-		  $('input[type="text"],input[type="password"]').css("box-shadow","0 0 3px red");
-		  alert("Please fill all fields...!!!!!!");
-		}	
-
+    $("#login").click(function()
+    {
+        var uid = $("#uid").val();
+        var password = $("#password").val();
+        
+        //checking for blank fields
+        if( uid =='' || password =='')
+        {
+            $('input[type="text"],input[type="password"]').css("border","2px solid red");
+            $('input[type="text"],input[type="password"]').css("box-shadow","0 0 3px red");
+            alert("Please fill all fields...!!!!!!");
+        }	
         else 
         {
+            if ( password.length < 8 )
+            {
+                $('input[type="password"]').css("border","2px solid red");
+                $('input[type="password"]').css("box-shadow","0 0 3px red");
+                alert(  'password must be of minimum 8 characters');
+                    $('#password').val('');
+            }
+            else {
+                alert('Welcome  '+ uid + '!!!');
+            }   
+        }
+    });
 
-    if ( password.length < 8 )
-    {
+    $("input").focus(function(){
+        $(this).css("background-color","#cccccc");
+        $(this).css("color","#000");
+    });
 
-       	  $('input[type="password"]').css("border","2px solid red");
-		  $('input[type="password"]').css("box-shadow","0 0 3px red");
-			alert(  'password must be of minimum 8 characters');
-              $('#password').val('');
-    }
-    
- 
-
- else {
- 
-
-			alert('Welcome  '+ uid + '!!!');
-		}   
-
-
-
-	}
-	});
-
-$("input").focus(function(){
-    $(this).css("background-color","#cccccc");
-     $(this).css("color","#000");
-  });
-  
-  $("input").blur(function(){
-    $(this).css("background-color","#ffffff");
-    $(this).css("color"," #4f4f4f"); 
-  });
+    $("input").blur(function(){
+        $(this).css("background-color","#ffffff");
+        $(this).css("color"," #4f4f4f"); 
+        });
 });
